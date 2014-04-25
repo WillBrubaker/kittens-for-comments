2259761c3ff6aa57f757b16c13b612988ad32108
Will <will@willthewebmechanic.com>
2014-04-25 18:29:43 -0500

    initial commit

---
+++ js/kittens4comments.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($) {
+var kittenShown = false;
+
+    $('#commentform').submit( function(e) {
+        if ( !(kittenShown) ) {
+            e.preventDefault();
+            $.colorbox({
+                href: kittenPic,
+                maxWidth: "80%",
+                maxHeight: "80%",
+                title: "Thanks for your comment! Here's your kitten.",
+                type: "photo",
+                onClosed: function(){
+                    kittenShown = true;
+                    HTMLFormElement.prototype.submit.call($('#commentform')[0]);
+                }
+            });
+            return false;
+        }
+    });
+
+    //waypoint stuff
+	$('#commentform').waypoint(function() {
+ 	$('.kittenpanel').show(400);
+	}, { offset: '110%' })
+
+
+});
